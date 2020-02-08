7aec760ed0f071974bd63f76a6bf75181f54deca
admin <admin@192.168.2.13>
2020-02-09 01:14:09 +0530

    add 2nd commit CollectionDummy.java for test purpose

---
+++ coreJava/src/com/collection/harendra/CollectionDummy.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.collection.harendra;
+
+/**
+ * @author Harendra Kumar Sah
+ *
+ */
+public class CollectionDummy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey Dummy.... for add commmit 2nd commit on git");
+
+	}
+
+}
